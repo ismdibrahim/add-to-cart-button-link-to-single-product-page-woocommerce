c1f700a3629e1469044b25293fb662ec37823b9c
Ibrahim Hossen <48528670+ismdibrahim@users.noreply.github.com>
2023-09-09 18:54:05 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,7 @@
+add_filter('woocommerce_loop_add_to_cart_link', 'custom_quick_add_to_cart_button');
+
+function custom_quick_add_to_cart_button($link) {
+    global $product;
+    $product_link = $product->get_permalink();
+    return '<a href="' . esc_url($product_link) . '" rel="nofollow" class="view_btn">View</a>';
+}
